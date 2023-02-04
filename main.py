f5691c9c2817bca06323b16c21bdbf923efa40e4
sungohki <sokim1201@naver.com>
2023-02-04 16:21:23 +0900

    fix : main.py fixed (subtract op added)

--- main.py
+++ main.py
@@ -6,3 +6,10 @@ num2 = int(input("Plz input num2 : "))
 res = num1 + num2
 
 print(res)
+
+num1 = int(input("input num1 : "))
+num2 = int(input("input num2 : "))
+
+res = num1 - num2
+
+print(res)
--- subtract.py
+++
@@ -1,5 +0,0 @@
-num1 = (int)(input("num1 : "))
-num2 = (int)(input("num2 : "))
-num3 = num1 - num2
-
-print(num3)
